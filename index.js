1434d38603e426c2dda5ca4a975a6f119d5c8a50
Eduardo Varela <dudusmyk7809@gmail.com>
2025-11-25 20:04:45 -0300

    Aplicação Node criada

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Aplicação rodando CI/CD!");
+  res.send("Aplicação rodando via CI/CD!");
 });
 
 const port = 3000;
-app.listen(port, () => console.log("Server on port " + port));
+app.listen(port, () => console.log("Servidor rodando na porta " + port));
 
 module.exports = app;
