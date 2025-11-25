750e88478cd8338cf7a5c56ee64cdae661029fa2
Eduardo Varela <dudusmyk7809@gmail.com>
2025-11-25 20:40:25 -0300

    Nova mensagem

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Aplicação rodando via CI/CD!");
+  res.send("Nova versão via CI/CD automatizado!");
+
 });
 
 const port = 3000;
